c71d81c90a4ec4859cc8c6d1f539f96306c4f0ce
ali <ali.mozafari733@yahoo.com>
2023-04-06 01:34:03 +0430

    hidden category

--- js/CategoryView.js
+++ js/CategoryView.js
@@ -3,22 +3,30 @@ import Storage from "./Storage.js";
 const categoryTitle = document.querySelector("#category-title");
 const categoryDescription = document.querySelector("#category-description");
 const addNewCetgoryBtn = document.querySelector("#add-new-category");
-const toggleaddcategorybtn=document.getElementById("toggle-add-category")
-const categorywrapper=document.getElementById("category-wrapper")
+const toggleaddcategorybtn = document.getElementById("toggle-add-category");
+const categorywrapper = document.getElementById("category-wrapper");
+const canceladdcategory = document.getElementById("canceladdcategory");
+
 class CategoryView {
   constructor() {
-    addNewCetgoryBtn.addEventListener("click", (e)=> this.addNewCategory());
-    toggleaddcategorybtn.addEventListener("click",(e)=>this.toggleaddcategory(e))
+    addNewCetgoryBtn.addEventListener("click", (e) => this.addNewCategory());
+    toggleaddcategorybtn.addEventListener("click", (e) =>
+      this.toggleaddcategory(e)
+    );
+    canceladdcategory.addEventListener("click", (e) =>
+      this.canceladdcategory(e)
+    );
     this.categories = [];
   }
   addNewCategory() {
-    alert("add category")
+    alert("add category");
     const title = categoryTitle.value;
     const description = categoryDescription.value;
     if (!title || !description) return;
     Storage.savecategory({ title, description });
     this.categories = Storage.getAllCategories();
     this.createCategoriesList();
+    categorywrapper.classList.add("hidden");
   }
   setApp() {
     this.categories = Storage.getAllCategories();
@@ -30,9 +38,11 @@ class CategoryView {
     });
     document.getElementById("product-category").innerHTML = result;
   }
-  toggleaddcategory(e){
-    categorywrapper.classList.toggle("hidden")
-   
+  toggleaddcategory(e) {
+    categorywrapper.classList.toggle("hidden");
+  }
+  canceladdcategory(e) {
+    categorywrapper.classList.add("hidden");
   }
 }
 export default new CategoryView();
